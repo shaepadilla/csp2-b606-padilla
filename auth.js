5073f4a179e545c88ea8d44f129bfb773daa99eb
shaepadilla <shaepadilla5@mail.com>
2026-04-23 22:49:49 +0800

    s51still

--- auth.js
+++ auth.js
@@ -1,74 +1,89 @@
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
+require('dotenv').config();
 
-// Create Access Token
 module.exports.createAccessToken = (user) => {
+	const data = {
+		id: user._id,
+		email: user.email,
+		isAdmin: user.isAdmin
+	}
 
-    const data = {
-        id: user._id,
-        email: user.email,
-        isAdmin: user.isAdmin
-    };
+	return jwt.sign(data, process.env.JWT_SECRET_KEY, {});
+}
 
-    return jwt.sign(data, process.env.JWT_SECRET_KEY, {});
-};
-
-
-// Verify Token
 module.exports.verify = (req, res, next) => {
 
-    let token = req.headers.authorization;
+	console.log(req.headers.authorization);
 
-    if (typeof token === "undefined") {
-        return res.status(403).send({
-            auth: "Failed",
-            message: "No Token Provided"
-        });
-    }
-
-    token = token.slice(7, token.length);
-
-    jwt.verify(token, process.env.JWT_SECRET_KEY, function(err, decodedToken){
-
-        if(err){
-            return res.status(403).send({
-                auth: "Failed",
-                message: err.message
-            });
-        }
-
-        req.user = decodedToken;
-        next();
-
-    });
-};
+	let token = req.headers.authorization;
 
 
-// Verify Admin
+	if (typeof token === "undefined") {
+
+		return res.send({auth: "Failed. No Token."})
+
+	} else {
+
+		console.log(token);
+
+		token = token.slice(7);
+
+		console.log(token);
+
+		jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decodedToken) => {
+
+			if (err) {
+				return res.status(403).send({
+					auth: "Failed",
+					message: err.message
+				})
+
+			} else {
+				console.log("result from verify method:");
+				console.log(decodedToken);
+
+				req.user = decodedToken;
+
+				next();
+
+			}
+		})
+	}
+
+}
+
+
 module.exports.verifyAdmin = (req, res, next) => {
+	if (req.user.isAdmin) {
+		next();
+	} else {
+		return res.status(403).send({
+			auth: "Failed",
+			message: "Action Forbidden"
+		})
+	}
+}
 
-    if(req.user.isAdmin){
+module.exports.errorHandler = (err, req, res, next) => {
+
+	console.error(err);	
+
+	const statusCode = err.status || 500;
+	const errorMessage = err.message || 'Internal Server Error';
+
+	res.status(statusCode).json({
+		error: {
+			message: errorMessage,
+			errorCode: err.code || 'SERVER ERROR',
+			details: err.details || null
+		}
+	})
+}
+
+module.exports.isLoggedIn = (req, res, next) => {
+    if (req.user) {
         next();
     } else {
-        return res.status(403).send({
-            auth: "Failed",
-            message: "Action Forbidden"
-        });
+        res.sendStatus(401);
     }
-
-};
-
-
-// Verify User Access
-module.exports.verifyUserAccess = (req, res, next) => {
-
-    if(req.user.isAdmin){
-        return res.status(403).send({
-            auth: "Failed",
-            message: "Action Forbidden"
-        });
-    }
-
-    next();
-
-};
+}
